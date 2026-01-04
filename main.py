59cfc4ec2ea2e655cadb1b5eab4ea1cc37619eef
kofronadam <kofikofiadam@gmail.com>
2026-01-04 21:04:55 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+import random
+import math
+import pygame
+from os import listdir
+from os.path import isfile, join
+pygame.init()
+
+pygame.display.set_caption("Platformer Game")
+
+BG_COLOR = (255, 255, 255)
+WIDTH, HEIGHT = 1000, 800
+FPS = 60
+PLAYER_VEL = 5
+
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+
+def main(window):
+    pass
+
+
+
+if __name__ == "__main__":
+    main(window)
