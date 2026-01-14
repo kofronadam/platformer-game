fa480ec69dfd3d9f4d54260cc035692dd8c02468
kofronadam <kofikofiadam@gmail.com>
2026-01-14 20:58:31 +0100

    img - directions

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def load_sprite_sheets(dir1, dir2, width, height, direction=False):
         if direction:
             all_sprites[img.replace(".png", "") + "_right"] = sprites
             all_sprites[img.replace(".png", "") + "_left"] = flip(sprites)
-
+            
 
 class Player(pygame.sprite.Sprite):
     COLOR = (255, 0, 0)
