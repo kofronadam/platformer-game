bec64187e14f8a1789f528871b9a9acb840cf645
kofronadam <kofikofiadam@gmail.com>
2026-01-16 21:51:37 +0100

    class player

--- main.py
+++ main.py
@@ -44,6 +44,7 @@ def load_sprite_sheets(dir1, dir2, width, height, direction=False):
 class Player(pygame.sprite.Sprite):
     COLOR = (255, 0, 0)
     GRAVITY = 1
+    SPRITES = load_sprite_sheets('MainCharacters', 'MaskDude', 32, 32, True)
 
     def __init__(self, x, y, width, height):
         self.rect = pygame.Rect((x, y, width, height))
@@ -75,13 +76,14 @@ class Player(pygame.sprite.Sprite):
             self.animation_count = 0
 
     def loop(self, fps):
-        self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY)
+        #self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY)
         self.move(self.x_vel, self.y_vel)
 
         self.fall_count += 1    
 
     def draw(self, win):
-        pygame.draw.rect(window, self.COLOR, self.rect)
+        self.sprite = self.SPRITES["idle"][0]
+        win.blit(self.sprite, (self.rect.x, self.rect.y))
 
 
 
