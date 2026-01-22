283d7bddecc76595501a51ef75ef1cc201625ef2
kofronadam <kofikofiadam@gmail.com>
2026-01-22 19:02:35 +0100

    terrain

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def get_block(size):
 class Player(pygame.sprite.Sprite):
     COLOR = (255, 0, 0)
     GRAVITY = 1
-    SPRITES = load_sprite_sheets('MainCharacters', 'MaskDude', 32, 32, True)
+    SPRITES = load_sprite_sheets('MainCharacters', 'VirtualGuy', 32, 32, True)
     ANIMATION_DELAY = 3
 
     def __init__(self, x, y, width, height):
@@ -292,12 +292,32 @@ def main(window):
     block_size = 96
 
     player = Player(100, 100, 50, 50)
-    fire = Fire(100, HEIGHT - block_size - 64, 16, 32)
+    fire = Fire(-480, HEIGHT - block_size - 64, 16, 32)
+    fire2 = Fire(-1050, HEIGHT - block_size - 64, 16, 32)
+
+
     fire.on()
+    fire2.on()
     floor = [Block(i * block_size, HEIGHT - block_size, block_size) 
              for i in range(-WIDTH // block_size, (WIDTH * 2) // block_size)]
-    objects = [*floor, Block(0, HEIGHT - block_size * 2, block_size), Block(block_size * 3, HEIGHT - block_size * 4, block_size), fire]
+    objects = [*floor, Block(0, HEIGHT - block_size * 2, block_size),
+               Block(block_size * 3, HEIGHT - block_size * 4, block_size), Block(block_size * 4, HEIGHT - block_size * 4, block_size), Block(block_size * 5, HEIGHT - block_size * 4, block_size), Block(block_size * 6, HEIGHT - block_size * 4, block_size), Block(block_size * 7, HEIGHT - block_size * 4, block_size),
+               Block(block_size * 7, HEIGHT - block_size * 3, block_size), 
+               Block(block_size * 10, HEIGHT - block_size * 3, block_size), Block(block_size * 11, HEIGHT - block_size * 3, block_size),
+               Block(block_size * 1, HEIGHT - block_size * 7, block_size), Block(block_size * 0, HEIGHT - block_size * 7, block_size),
+               Block(block_size * 20, HEIGHT - block_size * 2, block_size), Block(block_size * 20, HEIGHT - block_size * 3, block_size), Block(block_size * 20, HEIGHT - block_size * 4, block_size), Block(block_size * 20, HEIGHT - block_size * 5, block_size), Block(block_size * 20, HEIGHT - block_size * 7, block_size), Block(block_size * 20, HEIGHT - block_size * 8, block_size), Block(block_size * 20, HEIGHT - block_size * 9, block_size),
+               Block(block_size * -12, HEIGHT - block_size * 2, block_size), Block(block_size * -12, HEIGHT - block_size * 3, block_size), Block(block_size * -12, HEIGHT - block_size * 4, block_size), Block(block_size * -12, HEIGHT - block_size * 5, block_size),
+               Block(block_size * -9, HEIGHT - block_size * 3, block_size), Block(block_size * 20, HEIGHT - block_size * 3, block_size),
+               Block(block_size * -13, HEIGHT - block_size * 6, block_size), Block(block_size * -14, HEIGHT - block_size * 6, block_size),  Block(block_size * -17, HEIGHT - block_size * 7, block_size), Block(block_size * -18, HEIGHT - block_size * 6, block_size),
+               Block(block_size * -13, HEIGHT - block_size * 1, block_size), Block(block_size * -14, HEIGHT - block_size * 1, block_size), Block(block_size * -15, HEIGHT - block_size * 1, block_size), Block(block_size * -16, HEIGHT - block_size * 1, block_size),
+               Block(block_size * -20, HEIGHT - block_size * 1, block_size), Block(block_size * -22, HEIGHT - block_size * 3, block_size), Block(block_size * -20, HEIGHT - block_size * 5, block_size),
+               Block(block_size * -2, HEIGHT - block_size * 3, block_size), Block(block_size * -3, HEIGHT - block_size * 3, block_size), Block(block_size * -1, HEIGHT - block_size * 4, block_size),
+               Block(block_size * -6, HEIGHT - block_size * 5, block_size), Block(block_size * -7, HEIGHT - block_size * 5, block_size), Block(block_size * -8, HEIGHT - block_size * 5, block_size),
+               Block(block_size * -6, HEIGHT - block_size * 3, block_size), Block(block_size * -6, HEIGHT - block_size * 2, block_size),
 
+
+               fire, fire2 ]
+               
     offset_x = 0
     scroll_area_width = 200
 
